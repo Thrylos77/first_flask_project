65e9c7c709d9487522f656e71496893fc53f4041
Thrylos77 <125159447+Thrylos77@users.noreply.github.com>
2023-02-11 22:44:28 +0100

    first commit

---
+++ website/__init__.py
@@ -0,0 +1,48 @@
+from urllib.parse import quote_plus
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from os import path
+from flask_login import LoginManager
+from urllib.parse import quote_plus
+
+db = SQLAlchemy()
+DB_NAME = "database2.db"
+password = quote_plus('')
+# database_path = 'postgresql://{}:{}@{}/{}'.format('postgres', 'post', 'localhost:5432', 'devoir_python')
+database_path = f"sqlite:///{DB_NAME}"
+
+
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'Development'
+    app.config['SQLALCHEMY_DATABASE_URI'] = database_path
+    db.init_app(app)
+
+    from .views import views
+    from .auth import auth
+
+    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/')
+
+    from .models import User, Note
+    
+    with app.app_context():
+        db.create_all()
+
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
+
+    return app
+
+
+def create_database(app):
+    if not path.exists('website/' + DB_NAME):
+        db.create_all(app=app)
+        print('Created Database!')
